f24016524eec91a4286a6dae3e40f47e28ae7eb4
Ayoub Ziate <ayoub.ziate@artefact.com>
2023-06-21 09:32:36 +0000

    update tables config

--- models/staging/google_analytics/stg_google_analytics__events.sql
+++ models/staging/google_analytics/stg_google_analytics__events.sql
@@ -1,39 +1,49 @@
-{{
-  config(
-    materialized="view"
-  )
-}}
+{{ config(materialized="view") }}
 
-with source as (
-    
-    select 
-      *,
-      _table_suffix as table_suffix 
-    from {{ source('google_analytics', 'events_20210131') }}
-    where
-        -- start date (using the _table_suffix_ pseudo column for performance)
-        (_table_suffix between format_date('%Y%m%d', date('{{ var("start_date") }}'))
-            and format_date('%Y%m%d', current_date()))
-    
-),
+with
+    source as (
 
-renamed as (
-    
-    select
-      user_pseudo_id as fpc_id, -- first-party cookie-id
-      concat(user_pseudo_id, '.', (select cast(value.int_value as string) from unnest(event_params) where key = 'ga_session_id')) as session_id,
-      ifnull((select value.string_value from unnest(event_params) where key = 'traffic_type'), 'production') as traffic_type,
-      *
-    from source
+        select *, _table_suffix as table_suffix
+        from {{ source("google_analytics", "events") }}
+        where
+            -- start date (using the _table_suffix_ pseudo column for performance)
+            (
+                _table_suffix between format_date(
+                    '%Y%m%d', date('{{ var("start_date") }}')
+                ) and format_date('%Y%m%d', current_date())
+            )
 
-),
+    ),
 
-filtered as (
-  select 
-    *
-  from renamed
-  where
-    traffic_type not in ('development', 'internal')
-)
+    renamed as (
 
-select * from filtered
+        select
+            user_pseudo_id as fpc_id,  -- first-party cookie-id
+            concat(
+                user_pseudo_id,
+                '.',
+                (
+                    select cast(value.int_value as string)
+                    from unnest(event_params)
+                    where key = 'ga_session_id'
+                )
+            ) as session_id,
+            ifnull(
+                (
+                    select value.string_value
+                    from unnest(event_params)
+                    where key = 'traffic_type'
+                ),
+                'production'
+            ) as traffic_type,
+            *
+        from source
+
+    ),
+
+    filtered as (
+        select * from renamed where traffic_type not in ('development', 'internal')
+    )
+
+select *
+from filtered
